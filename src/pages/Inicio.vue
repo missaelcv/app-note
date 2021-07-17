11a1d8ab46d6f0d8df57b496b708fbfc36aa93d5
missaelcv <missaelcv0220@gmail.com>
2021-07-17 00:05:32 -0400

    changer ajustes and inicio

---
+++ src/pages/Inicio.vue
@@ -0,0 +1,28 @@
+<template>
+<q-page padding>
+   <div class="q-pa-md">
+    <div class="q-gutter-md">
+      <q-time v-model="time" />
+
+      <q-time
+        v-model="timeWithSeconds"
+        with-seconds
+      />
+    </div>
+  </div>
+  </q-page>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    
+    return {
+        time: ref('10:56'),
+      timeWithSeconds: ref('09:24:10')
+    }
+  }
+}
+</script>
